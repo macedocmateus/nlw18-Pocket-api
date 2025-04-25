bb9695e0db5ce3680c9fff24aff4d86fadd8f2ea
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-04-25 18:07:54 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import express from "express"
+import express from "express";
 
-import { routes } from "./routes"
-import { errorHandling } from "./middlewares/error-handling"
+import { routes } from "./routes";
+import { errorHandling } from "./middlewares/error-handling";
+import cors from "cors";
 
-const PORT = 3333
-const app = express()
+const PORT = 3333;
+const app = express();
 
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.use(errorHandling)
+app.use(errorHandling);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
